665bac97b4d0d15dbd6e10b1897c082bc255f0b1
!-! <13162153278@163.com>
2018-12-29 11:13:59 +0800

    no message

--- ProjectTemplate/ProjectTemplate/Extensions/Bundle-Extension.swift
+++ ProjectTemplate/ProjectTemplate/Extensions/Bundle-Extension.swift
@@ -11,4 +11,14 @@ extension Bundle {
     var apiBaseURL: String {
         return object(forInfoDictionaryKey: "serverBaseURL") as? String ?? ""
     }
+    var timeoutInterval: TimeInterval {
+        return object(forInfoDictionaryKey: "timeoutInterval") as? TimeInterval ?? 0
+    }
+    var applicationAPI: Dictionary<String, String> {
+        return object(forInfoDictionaryKey: "ApplicationAPI") as! Dictionary<String, String>
+    }
+    var apiLogin: String {
+        return applicationAPI["login"] ?? ""
+    }
+    
 }
